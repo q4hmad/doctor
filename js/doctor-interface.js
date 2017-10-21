edfe05ec9dc9de81660b9d7f58a305d9cb3eaabc
Qudsia Ahmad <q4hmad@gmail.com>
2017-10-21 11:28:07 -0700

    update

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,15 +1,42 @@
-import {DoctorData } from './../js/doctor.js';
-let apiKey = require('./../.env').apiKey;
+
 
 $(document).ready(function() {
   $("#userInput").submit(function(event) {
     event.preventDefault();
-    let doctor = new DoctorData();
+
     let issue  = $("#issue").val();
-    let docName  = $("#name").val();
-    let promise = doctor.makePromise(`https://api.betterdoctor.com/2016-03-01/doctors?location=%20or-portland&user_location=37.773%2C-122.413&skip=0&limit=5&user_key=${apiKey}`);
 
 
-    doctor.callApi(promise, issue, docName, apiKey);
+    let promise =  new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&name=${name}&location=%20or-portland&user_location=37.773%2C-122.413&skip=0&limit=5&user_key=1288d09ed7e0e1d2b0242cfdd73d98c7`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      let data = (body.data);
+      data.forEach(function(info) {
+        $('#resultIssue').text(`The doctors who treat ${issue} are ${info.practices}`);
+        console.log(info.practices);
+
+        $('#resultName').text(`Doctors with the name ${name} ${info.specialty_uid}`);
+
+
+
+
+
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
  });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,34 +1,10 @@
-export class DoctorData {
-  constructor() {
-
-  }
 
 
 
 
-makePromise(apiKey){
-  return new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    let url = apiKey;
-    request.onload = function() {
-      if (this.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(Error(request.statusText));
-      }
-    };
-    request.open("GET", url, true);
-    request.send();
-  });
-}
 
-callApi(promise,issue,docName) {
-  promise.then(function(response) {
-    let body = JSON.parse(response);
-          $("#resultIssue").append(`Issue: ${body.data.practices}`);
-          $("#resultName").append(`Name: ${body.data.practices}`)
-        }, function(error) {
-         $('#showErrors').text(`There was an error processing your request: ${error.message}`);
-      });
-    }
-  }//end of class
+
+
+
+
+  
