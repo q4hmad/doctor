f96a9043fcbc29dbaa04ff31c72d4e72814b98a3
Guest User <Guest@Epicodus-6A.local>
2017-10-23 13:25:30 -0700

    errors fixed

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,4 +1,4 @@
-
+apiKey = require('./../.env').apiKey
 
 $(document).ready(function() {
   $("#userInput").submit(function(event) {
@@ -8,7 +8,7 @@ $(document).ready(function() {
     let docName  = $("#name").val();
     let promise =  new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&&location=45.4814320%2C-122.8016600%2C50&skip=0&limit=10&user_key=${appKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&&location=45.4814320%2C-122.8016600%2C50&skip=0&limit=10&user_key=${apiKey}`;
 
       request.onload = function() {
         if (this.status === 200) {
@@ -33,16 +33,8 @@ $(document).ready(function() {
         // console.log(newPatients);
         let phone = doctor.practices[0].phones;
         console.log(phone);
-        $('#resultIssue').append(name);
-        $("#resultAddress").append(`${practices.street} ${practices.city} ${practices.state}`);
-        $("#resultPhoneNumber").append(phone);
-        $("#resultNewPatient").append(newPatients);
-      } else {
-        $("#resultIssue").text(`There are no results for this ${issue}. Please try again using different search terms.`);
 
-      }, (function(error) {
-      $('#showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
+    })
    });
   });
 });
