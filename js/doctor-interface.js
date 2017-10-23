f3c1e454b4cce441c3c3d7dc6ad4d027102bd814
Qudsia Ahmad <q4hmad@gmail.com>
2017-10-22 20:53:01 -0700

    all basic information

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     let docName  = $("#name").val();
     let promise =  new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&&location=45.4814320%2C-122.8016600%2C50&skip=0&limit=10&user_key=1288d09ed7e0e1d2b0242cfdd73d98c7`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&&location=45.4814320%2C-122.8016600%2C50&skip=0&limit=10&user_key=${appKey}`;
 
       request.onload = function() {
         if (this.status === 200) {
@@ -28,12 +28,20 @@ $(document).ready(function() {
         let profile = doctor.profile;
         let name = profile.first_name + ' ' + profile.last_name + " " + profile.title;
         let practices = doctor.practices[0].visit_address;
-        console.log(practices);
+        // console.log(practices);
+        let newPatients = doctor.practices[0].accepts_new_patients;
+        // console.log(newPatients);
+        let phone = doctor.practices[0].phones;
+        console.log(phone);
         $('#resultIssue').append(name);
         $("#resultAddress").append(`${practices.street} ${practices.city} ${practices.state}`);
+        $("#resultPhoneNumber").append(phone);
+        $("#resultNewPatient").append(newPatients);
+      } else {
+        $("#resultIssue").text(`There are no results for this ${issue}. Please try again using different search terms.`);
 
-    }), (function(error) {
-      $('#showErrors').text('No results');
+    }, (function(error) {
+      $('#showErrors').text(`There was an error processing your request: ${error.message}`);
     });
    });
   });
