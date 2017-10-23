9ab2b275ece50d3874181e569dcea8f9abc0cbd4
Qudsia Ahmad <q4hmad@gmail.com>
2017-10-22 18:37:05 -0700

    showing names

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     let docName  = $("#name").val();
     let promise =  new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${docName}&skip=0&limit=10&user_key=1288d09ed7e0e1d2b0242cfdd73d98c7`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&skip=0&limit=10&user_key=1288d09ed7e0e1d2b0242cfdd73d98c7`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -23,15 +23,14 @@ $(document).ready(function() {
     promise.then(function(response) {
       let body = JSON.parse(response);
       let data = body.data;
-      console.log(data);
       data.forEach(function(doctor) {
         let profile = doctor['profile'];
         let name = profile.first_name + ' ' + profile.last_name;
-        console.log(profile);
-        $("#resultsIssue").issue(profile.first_name)
-      });
-    }, function(error) {
-      $('.showErrors').text('No results');
+        console.log(name);
+      $('#resultIssue').append(name);
+    }), (function(error) {
+      $('#showErrors').text('No results');
     });
+   });
   });
 });
